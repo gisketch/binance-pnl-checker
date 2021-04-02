f4987a4a3aed13351480c4c17c8074663a7a93da
gisketch <agjmnz@gmail.com>
2021-04-03 00:57:24 +0800

    refreshing data from API to html + css update

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 "use strict";
 
+/// ------- UNIQUE ID ----------///
+
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
 ///------- COINS CLASSES --------///
 
 class Coin {
-  constructor(symbol, orderPrice, prevDay, price) {
-    this.symbol = symbol;
+  constructor(symbol, orderPrice, orderCurrency) {
+    this.symbol = symbol.toUpperCase();
     this.orderPrice = orderPrice; //price when bought
-    this.prevDay = prevDay; //price last day
-    this.price = price; //current price (should update every second thru API)
-
-    this._setPnl();
-  }
-
-  _setPnl() {
-    this.pnl = ((this.price - this.orderPrice) / this.orderPrice) * 100;
+    this.orderCurrency = orderCurrency.toFixed(2); //price when bought
+    this.coinId = "a";
   }
 }
 
@@ -21,7 +25,8 @@ class Coin {
 const pnlContainer = document.querySelector(".pnl-container");
 const addCoin = document.querySelector(".btn");
 const inputSymbol = document.querySelector(".input-symbol");
-const inputOrder = document.querySelector(".order-price");
+const inputOrder = document.querySelector(".input-order-price");
+const inputOrderCurrency = document.querySelector(".input-order-currency");
 let coin;
 
 class App {
@@ -40,9 +45,9 @@ class App {
 
     const coinSymbol = inputSymbol.value;
     const coinOrder = +inputOrder.value;
+    const coinOrderCurrency = +inputOrderCurrency.value;
 
-    coin = new Coin(coinSymbol, coinOrder, 2.456, 3.569); //PLACEHOLDER prevday and price
-
+    coin = new Coin(coinSymbol, coinOrder, coinOrderCurrency); //PLACEHOLDER prevday and price
     this.#coins.push(coin);
     this._renderCoin(coin);
   }
@@ -52,15 +57,56 @@ class App {
         <div class="coin">
           <div class="icon">+</div>
           <div class="coin-details">
-            <span class="coin-name">${coin.symbol}</span>
-            <span class="order-price">${coin.orderPrice}</span>
-            <span class="prev-price">${coin.prevDay}</span>
-            <span class="price">${coin.price}</span>
-            <span class="${coin.pnl > 0 ? "gain" : "loss"}">${coin.pnl}%</span>
+            <div class="coin-detail-1">
+              <span class="coin-name">${coin.symbol.toUpperCase()}</span>
+              <span class="order-price">${coin.orderPrice}</span>
+              <span class="order-currency">${coin.orderCurrency}</span>
+            </div>
+
+            <div class="coin-detail-2">
+              <span id="${coin.coinId}" class="price">Loading...</span>
+              <span id="coin-pnl" class="${
+                coin.pnl > 0 ? "gain" : "loss"
+              }">Loading...</span>
+              <span id="currency-pnl">Loading...</span>
+            </div>
           </div>
         </div>`;
 
     pnlContainer.insertAdjacentHTML("afterbegin", html);
+
+    let coinPrice = document.querySelector(`#${coin.coinId}`);
+    let pnlText = document.querySelector(`#coin-pnl`);
+    let curCurrency = document.querySelector(`#currency-pnl`);
+    let coinImage = document.querySelector(`.icon`);
+    let curPrice;
+    let newPnl;
+
+    fetch("https://cryptoicons.org/api/icon/eth/200"); //get crypto icon
+
+    ///--- following function will update every 500ms ---///
+    setInterval(function () {
+      fetch(
+        `https://api.binance.com/api/v3/ticker/price?symbol=${coin.symbol.toUpperCase()}`
+      )
+        .then((res) => res.json())
+        .then(function (data) {
+          const newPrice = +data.price;
+          coinPrice.textContent = newPrice.toPrecision(6);
+          curPrice = data.price;
+        })
+        .catch((err) => console.log(err));
+
+      ///---- set PnL percentage -----///
+      newPnl = ((curPrice - coin.orderPrice) / coin.orderPrice) * 100;
+      pnlText.textContent = `${newPnl >= 0 ? "+" : ""}${newPnl.toFixed(2)}%`;
+
+      ///----- set new Currency ---- ///
+      curCurrency.textContent = `${(
+        coin.orderCurrency *
+        (1 + newPnl / 100)
+      ).toFixed(2)}`;
+    }, 500);
   }
 
   _loadAllCoins(coinsArr) {
@@ -71,7 +117,3 @@ class App {
 }
 
 const app = new App();
-
-fetch("https://api.binance.com/api/v3/ticker/price?symbol=btcusdt ")
-  .then((res) => res.json())
-  .then((res) => console.log(res));
